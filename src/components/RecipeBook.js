7a903da25da11fae73e14398a792de6cd2b12fde
BrianLusina <lusinabrian@gmail.com>
2017-01-16 23:37:49 +0300

    add lifecycles

--- src/components/RecipeBook.js
+++ src/components/RecipeBook.js
@@ -2,6 +2,18 @@
 import React, { Component } from 'react';
 
 export default class RecipeBook extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentWillRecieveProps(){
+
+    }
+    
+    shouldComponentUpdate(nextProps, nextState){
+
+    }
+
     render(){
         return(
             <div>
