3e213f33254fd534848e1bcceb969410006cc92f
BrianLusina <lusinabrian@gmail.com>
2017-01-20 22:26:35 +0300

    rename tests

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/components/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 describe("App component tests", function(){
   var div;
+  const AppComponent;
   beforeEach(()=>{
     div = document.createElement('div');
+    AppComponent = () => (
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    );
   });
 
   it('renders without crashing', () => {
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<AppComponent />, div);
   });
 
 });
